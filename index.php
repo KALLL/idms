2751b8c9e252ce362de35aff6b68967d24b8cff4
horizontechict <solutions@horizontechict.com>
2022-07-15 15:38:10 +0300

    css minified

--- index.php
+++ index.php
@@ -5,83 +5,79 @@
     <meta name="keywords" content="">
     <meta name="description" content="">
     <title>Home</title>
-    <link rel="stylesheet" href="nicepage.css" media="screen">
-<link rel="stylesheet" href="Home.css" media="screen">
+    <link rel="stylesheet" href="styles.pure.css" media="screen">
     <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
-    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
     <meta name="generator" content="Nicepage 4.13.4, nicepage.com">
     <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
-
-
     <script type="application/ld+json">{
-		"@context": "http://schema.org",
-		"@type": "Organization",
-		"name": ""
-}</script>
+        "@context": "http://schema.org",
+        "@type": "Organization",
+        "name": ""
+    }</script>
     <meta name="theme-color" content="#478ac9">
     <meta property="og:title" content="Home">
     <meta property="og:type" content="website">
-  </head>
-  <body data-home-page="Home.html" data-home-page-title="Home" class="u-body u-stick-footer u-xl-mode"><header class="u-clearfix u-custom-color-1 u-header u-header" id="sec-aa17"><div class="u-clearfix u-sheet u-valign-middle-sm u-valign-top-lg u-valign-top-xl u-sheet-1">
-        <div class="u-clearfix u-gutter-0 u-layout-wrap u-layout-wrap-1">
-          <div class="u-layout">
+</head>
+<body data-home-page="Home.html" data-home-page-title="Home" class="u-body u-stick-footer u-xl-mode"><header class="u-clearfix u-custom-color-1 u-header u-header" id="sec-aa17"><div class="u-clearfix u-sheet u-valign-middle-sm u-valign-top-lg u-valign-top-xl u-sheet-1">
+    <div class="u-clearfix u-gutter-0 u-layout-wrap u-layout-wrap-1">
+        <div class="u-layout">
             <div class="u-layout-col">
-              <div class="u-container-style u-layout-cell u-size-30 u-layout-cell-1">
-                <div class="u-container-layout u-valign-bottom-lg u-valign-bottom-md u-valign-bottom-sm u-valign-bottom-xl u-container-layout-1">
-                  <h6 class="u-align-right u-text u-text-default u-text-1">10:55 AM</h6>
+                <div class="u-container-style u-layout-cell u-size-30 u-layout-cell-1">
+                    <div class="u-container-layout u-valign-bottom-lg u-valign-bottom-md u-valign-bottom-sm u-valign-bottom-xl u-container-layout-1">
+                        <h6 class="u-align-right u-text u-text-default u-text-1">10:55 AM</h6>
+                    </div>
                 </div>
-              </div>
-              <div class="u-container-style u-layout-cell u-size-30 u-layout-cell-2">
-                <div class="u-container-layout u-container-layout-2">
-                  <h6 class="u-align-right u-text u-text-default u-text-2">13/07/2022</h6>
+                <div class="u-container-style u-layout-cell u-size-30 u-layout-cell-2">
+                    <div class="u-container-layout u-container-layout-2">
+                        <h6 class="u-align-right u-text u-text-default u-text-2">13/07/2022</h6>
+                    </div>
                 </div>
-              </div>
             </div>
-          </div>
         </div>
-        <h4 class="u-text u-text-default u-text-3">IDMS Client&nbsp;</h4>
-        <h4 class="u-text u-text-default u-text-4">IDMS</h4>
-        <div class="u-align-center-sm u-align-center-xs u-clearfix u-layout-wrap u-layout-wrap-2">
-          <div class="u-gutter-0 u-layout">
+    </div>
+    <h4 class="u-text u-text-default u-text-3">IDMS Client&nbsp;</h4>
+    <h4 class="u-text u-text-default u-text-4">IDMS</h4>
+    <div class="u-align-center-sm u-align-center-xs u-clearfix u-layout-wrap u-layout-wrap-2">
+        <div class="u-gutter-0 u-layout">
             <div class="u-layout-row">
-              <div class="u-container-style u-layout-cell u-size-30 u-layout-cell-3">
-                <div class="u-container-layout u-valign-middle-lg u-valign-middle-md u-valign-middle-xl u-container-layout-3">
-                  <h6 class="u-align-center-sm u-align-center-xs u-align-right-lg u-align-right-md u-align-right-xl u-text u-text-5"> Assembly Name 1</h6>
+                <div class="u-container-style u-layout-cell u-size-30 u-layout-cell-3">
+                    <div class="u-container-layout u-valign-middle-lg u-valign-middle-md u-valign-middle-xl u-container-layout-3">
+                        <h6 class="u-align-center-sm u-align-center-xs u-align-right-lg u-align-right-md u-align-right-xl u-text u-text-5"> Assembly Name 1</h6>
+                    </div>
                 </div>
-              </div>
-              <div class="u-container-style u-layout-cell u-size-30 u-layout-cell-4">
-                <div class="u-container-layout u-valign-middle-lg u-valign-middle-md u-valign-middle-xl u-container-layout-4">
-                  <h6 class="u-align-center-sm u-align-center-xs u-align-left-md u-text u-text-default-lg u-text-default-md u-text-default-sm u-text-default-xs u-text-6"> Station 21</h6>
+                <div class="u-container-style u-layout-cell u-size-30 u-layout-cell-4">
+                    <div class="u-container-layout u-valign-middle-lg u-valign-middle-md u-valign-middle-xl u-container-layout-4">
+                        <h6 class="u-align-center-sm u-align-center-xs u-align-left-md u-text u-text-default-lg u-text-default-md u-text-default-sm u-text-default-xs u-text-6"> Station 21</h6>
+                    </div>
                 </div>
-              </div>
             </div>
-          </div>
         </div>
-      </div></header>
-    <section class="u-clearfix u-palette-5-light-1 u-section-1" id="sec-d8d8">
-      <div class="u-clearfix u-sheet u-sheet-1">
+    </div>
+</div></header>
+<section class="u-clearfix u-palette-5-light-1 u-section-1" id="sec-d8d8">
+    <div class="u-clearfix u-sheet u-sheet-1">
         <img class="u-absolute-hcenter u-expanded-height u-image u-image-round u-preserve-proportions u-radius-10 u-image-1" src="images/016ea949819a90de23c0bdc93f25e826492e9c9210fefb273343f809983c460413fb7735d0a928f1df1852ab131d91c234b95d6ecc1ad743e84ab5_1280.jpg" alt="" data-image-width="1280" data-image-height="873">
-      </div>
-    </section>
+    </div>
+</section>
 
 
-    <footer class="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-3515"><div class="u-clearfix u-sheet u-sheet-1">
-        <div class="u-clearfix u-expanded-width u-layout-wrap u-layout-wrap-1">
-          <div class="u-layout">
+<footer class="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-3515"><div class="u-clearfix u-sheet u-sheet-1">
+    <div class="u-clearfix u-expanded-width u-layout-wrap u-layout-wrap-1">
+        <div class="u-layout">
             <div class="u-layout-row">
-              <div class="u-container-style u-layout-cell u-size-30 u-layout-cell-1">
-                <div class="u-container-layout u-valign-middle-lg u-valign-middle-md u-valign-middle-xl u-container-layout-1">
-                  <p class="u-align-center-xs u-small-text u-text u-text-variant u-text-1">This is RSS fied section.&nbsp;</p>
+                <div class="u-container-style u-layout-cell u-size-30 u-layout-cell-1">
+                    <div class="u-container-layout u-valign-middle-lg u-valign-middle-md u-valign-middle-xl u-container-layout-1">
+                        <p class="u-align-center-xs u-small-text u-text u-text-variant u-text-1">This is RSS fied section.&nbsp;</p>
+                    </div>
                 </div>
-              </div>
-              <div class="u-container-style u-layout-cell u-size-30 u-layout-cell-2">
-                <div class="u-container-layout u-valign-middle-lg u-valign-middle-md u-valign-middle-xl u-container-layout-2">
-                  <p class="u-align-center-lg u-align-center-md u-align-center-sm u-align-center-xs u-small-text u-text u-text-palette-2-base u-text-variant u-text-2">This is sample error. And shows detail of any error happened.</p>
+                <div class="u-container-style u-layout-cell u-size-30 u-layout-cell-2">
+                    <div class="u-container-layout u-valign-middle-lg u-valign-middle-md u-valign-middle-xl u-container-layout-2">
+                        <p class="u-align-center-lg u-align-center-md u-align-center-sm u-align-center-xs u-small-text u-text u-text-palette-2-base u-text-variant u-text-2">This is sample error. And shows detail of any error happened.</p>
+                    </div>
                 </div>
-              </div>
             </div>
-          </div>
         </div>
-      </div></footer>
-  
+    </div>
+</div></footer>
+
 </body></html>
